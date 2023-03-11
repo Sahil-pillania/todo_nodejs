d0ffa6f8bd7e9aca08854731ed0491b1e1fdb034
Sahil pillania <sahilpillania88@gmail.com>
2023-03-11 13:37:30 +0530

    dependencies added (parser)

--- index.js
+++ index.js
@@ -6,16 +6,21 @@ const main = require("./db");
 main();
 // app.use(express.static("/assets"));
 app.use(express.static(__dirname + "/assets"));
+// ejs
 let ejs = require("ejs");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.json());
 
-// app.get("/", (req, res) => {
-//   return res.render("Index");
-// });
+// body parser
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+// routes
 app.use("/", require("./routes/todo.js"));
 
+// port listening
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
